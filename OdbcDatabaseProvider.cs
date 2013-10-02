7efd26a7a7bb1cce0ef815ddf824f761bd69280e
kharnagy <kharnagy@inedo.com>
2013-10-02 11:30:27 -0400

    Initial commit

---
+++ OdbcDatabaseProvider.cs
@@ -0,0 +1,73 @@
+﻿using System.Data.Odbc;
+using Inedo.BuildMaster.Extensibility.Providers;
+using Inedo.BuildMaster.Extensibility.Providers.Database;
+using Inedo.BuildMaster.Web;
+
+namespace Inedo.BuildMasterExtensions.Odbc
+{
+    [ProviderProperties(
+        "ODBC (Limited)",
+        "Supports ODBC databases, but does not provide change script functionality; a more specific provider should be chosen when possible.")]
+    [CustomEditor(typeof(OdbcDatabaseProviderEditor))]
+    public sealed class OdbcDatabaseProvider : DatabaseProviderBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OdbcDatabaseProvider"/> class.
+        /// </summary>
+        public OdbcDatabaseProvider()
+        {
+        }
+
+        public override bool IsAvailable()
+        {
+            return true;
+        }
+        public override void ValidateConnection()
+        {
+            using (var conn = new OdbcConnection(this.ConnectionString))
+            {
+                conn.Open();
+                conn.Close();
+            }
+        }
+        public override void ExecuteQueries(string[] queries)
+        {
+            if (queries == null || queries.Length == 0)
+                return;
+
+            using (var conn = new OdbcConnection(this.ConnectionString))
+            {
+                conn.Open();
+
+                using (var cmd = new OdbcCommand(string.Empty, conn))
+                {
+                    foreach (var query in queries)
+                    {
+                        cmd.CommandText = query;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+        public override void ExecuteQuery(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+                return;
+
+            using (var conn = new OdbcConnection(this.ConnectionString))
+            {
+                conn.Open();
+
+                using (var cmd = new OdbcCommand(query, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public override string ToString()
+        {
+            var csb = new OdbcConnectionStringBuilder(this.ConnectionString);
+            return string.Format("ODBC Connection ({0})", csb.Driver);
+        }
+    }
+}
---
+++ OdbcDatabaseProviderEditor.cs
@@ -0,0 +1,55 @@
+﻿using Inedo.BuildMaster.Extensibility.Providers;
+using Inedo.BuildMaster.Web.Controls;
+using Inedo.BuildMaster.Web.Controls.Extensions;
+using Inedo.Web.Controls;
+
+namespace Inedo.BuildMasterExtensions.Odbc
+{
+    /// <summary>
+    /// Custom editor for the ODBC database provider.
+    /// </summary>
+    internal sealed class OdbcDatabaseProviderEditor : ProviderEditorBase
+    {
+        private ValidatingTextBox txtConnectionString;
+
+        public override void BindToForm(ProviderBase extension)
+        {
+            EnsureChildControls();
+
+            var mysql = (OdbcDatabaseProvider)extension;
+            txtConnectionString.Text = mysql.ConnectionString;
+        }
+
+        public override ProviderBase CreateFromForm()
+        {
+            EnsureChildControls();
+
+            return new OdbcDatabaseProvider
+            {
+                ConnectionString = txtConnectionString.Text
+            };
+        }
+
+        protected override void CreateChildControls()
+        {
+            this.txtConnectionString = new ValidatingTextBox
+            {
+                Width = 300,
+                Required = true
+            };
+
+            this.Controls.Add(
+                new FormFieldGroup(
+                    "Connection String",
+                    "The ODBC connection string. There are several formats for this, but two common ones are:<br /><br />"
+                    + "<em>Driver={My Driver Name}; some_attribute=some_value;</em><br /><br />"
+                    + "<em>DSN=dsn_name</em>",
+                    false,
+                    new StandardFormField(string.Empty, txtConnectionString)
+                )
+            );
+
+            base.CreateChildControls();
+        }
+    }
+}
